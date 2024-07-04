f2a18a6c26841d03fd1c105926e335f0d5e90afc
Aabdulloh2005 <ganievabdulloh2005@gmail.com>
2024-07-04 17:01:20 +0500

    Chat appp

---
+++ lib/controllers/chat_controller.dart
@@ -0,0 +1,33 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:lesson70/services/chat_sevice.dart';
+
+// class ChatController {
+//   final _chatService = ChatSevice();
+
+//   void sendMessage(String toUserId, String message, String userName) {
+//     _chatService.sendMessage(toUserId, message, userName);
+//   }
+
+//   Stream<QuerySnapshot> getMessages(String userId, String otherUserId) async* {
+//     yield* _chatService.getMessages(userId, otherUserId);
+//   }
+// }
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:lesson70/services/chat_sevice.dart';
+import 'dart:io';
+
+class ChatController {
+  final _chatService = ChatService();
+
+  void sendMessage(String toUserId, String message, String userName) {
+    _chatService.sendMessage(toUserId, message, userName);
+  }
+
+  void sendImageMessage(String toUserId, File imageFile, String userName) {
+    _chatService.sendImageMessage(toUserId, imageFile, userName);
+  }
+
+  Stream<QuerySnapshot> getMessages(String userId, String otherUserId) async* {
+    yield* _chatService.getMessages(userId, otherUserId);
+  }
+}
